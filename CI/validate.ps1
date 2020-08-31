6bd54f3cf3c7ef123da4e7c5159e82874eb7280a
Dmitry Serbin <dmitry.serbin@iag.co.nz>
2020-09-01 08:59:59 +1200

    Update CI

--- CI/validate.ps1
+++ CI/validate.ps1
@@ -2,102 +2,114 @@
 Param
 (
 	[Parameter(Mandatory=$True)]
-	[String]$Path
+	[String]$Path,
+
+	[Parameter(Mandatory=$False)]
+	[Switch]$Required
 )
 
 function Get-LocalExtension
 {
-    [CmdletBinding()]
-    Param
-    (
-        [Parameter(Mandatory=$True)]
-        [String]$Path
-    )
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$True)]
+		[String]$Path
+	)
 
-    if (-not (Test-Path -Path $Path))
-    {
-        throw "Directory <$Path> not found"
-    }
+	if (-not (Test-Path -Path $Path))
+	{
+		throw "Directory <$Path> not found"
+	}
 
-    $ExtensionFile = Get-ChildItem `
-        -Path $Path `
-        -Filter vss-extension.json `
-        -File `
-        -ErrorAction Stop | Select-Object -First 1
-        
-    if (-not (Test-Path -Path $ExtensionFile))
-    {
-        throw "File <$ExtensionFile> not found"
-    }
+	$ExtensionFile = Get-ChildItem `
+		-Path $Path `
+		-Filter vss-extension.json `
+		-File `
+		-ErrorAction Stop | Select-Object -First 1
 
-    $Extension = Get-Content `
-        -Path $ExtensionFile `
-        -ErrorAction Stop | ConvertFrom-Json `
-            -Depth 100 `
-            -ErrorAction Stop
+	if (-not (Test-Path -Path $ExtensionFile))
+	{
+		throw "File <$ExtensionFile> not found"
+	}
 
-    return $Extension
+	$Extension = Get-Content `
+		-Path $ExtensionFile `
+		-ErrorAction Stop | ConvertFrom-Json `
+			-Depth 100 `
+			-ErrorAction Stop
+
+	return $Extension
 }
 
 function Get-MarketplaceExtension
 {
-    [CmdletBinding()]
-    Param
-    (
-        [Parameter(Mandatory=$True)]
-        [String]$Name,
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$True)]
+		[String]$Name,
 
-        [Parameter(Mandatory=$True)]
-        [String]$ApiVersion
-    )
+		[Parameter(Mandatory=$True)]
+		[String]$ApiVersion
+	)
 
-    $Uri = ("https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery?api-version={0}" -f $ApiVersion)
+	$Uri = ("https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery?api-version={0}" -f $ApiVersion)
 
-    $Query = @{
-        filters = @(
-            @{
-                criteria = @(
-                    @{
-                        filterType = 7
-                        value = $Name
-                    }
-                )
-            }
-        )
-        flags = 103
-    }
-    
-    $Body = $Query | ConvertTo-Json `
-        -Depth 100 `
-        -Compress `
-        -ErrorAction Stop
+	$Query = @{
+		filters = @(
+			@{
+				criteria = @(
+					@{
+						filterType = 7
+						value = $Name
+					}
+				)
+			}
+		)
+		flags = 103
+	}
 
-    $Extension = (Invoke-RestMethod `
-        -Method Post `
-        -Uri $Uri `
-        -ContentType application/json `
-        -Body $Body `
-        -ErrorAction Stop).results.extensions[0].versions[0]
+	$Body = $Query | ConvertTo-Json `
+		-Depth 100 `
+		-Compress `
+		-ErrorAction Stop
 
-    if (-not $Extension)
-    {
-        throw "Extension <$Name> not found"
-    }
+	$Extension = (Invoke-RestMethod `
+		-Method Post `
+		-Uri $Uri `
+		-ContentType application/json `
+		-Body $Body `
+		-ErrorAction Stop).results.extensions[0].versions[0]
 
-    return $Extension
+	if (-not $Extension)
+	{
+		throw "Extension <$Name> not found"
+	}
+
+	return $Extension
 }
 
 $ReleaseExtension = Get-LocalExtension `
-    -Path $Path
+	-Path $Path
 
 $LatestExtension = Get-MarketplaceExtension `
-    -Name ("{0}.{1}" -f $ReleaseExtension.publisher, $ReleaseExtension.id) `
-    -ApiVersion 6.1-preview.1
+	-Name ("{0}.{1}" -f $ReleaseExtension.publisher, $ReleaseExtension.id) `
+	-ApiVersion 6.1-preview.1
 
 Write-Host ("Latest extension version: {0}" -f $LatestExtension.version)
 Write-Host ("Release extension version: {0}" -f $ReleaseExtension.version)
 
 if ($ReleaseExtension.version -le $LatestExtension.version)
 {
-    throw "Extension version <$($ReleaseExtension.version)> cannot be released"
+	$ErrorMessage = "Extension version <$($ReleaseExtension.version)> cannot be released"
+
+	if ($Required)
+	{
+		throw $ErrorMessage
+	}
+	else
+	{
+		Write-Warning $ErrorMessage
+	}
 }
