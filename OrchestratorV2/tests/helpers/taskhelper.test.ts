32ef9305d46c372ee1dbebfb993fa4b0c0d78232
Dmitry Serbin <dmitry.serbin@iag.co.nz>
2020-09-15 12:33:03 +1200

    Add tests

--- OrchestratorV2/tests/helpers/taskhelper.test.ts
+++ OrchestratorV2/tests/helpers/taskhelper.test.ts
@@ -29,10 +29,16 @@ describe("TaskHelper", ()  => {
     const definitionNameMock: string = "My-Definition";
     const releaseNameMock: string = "My-Release";
     const releaseStagesMock: string = "DEV,TEST,PROD";
+
+    const releaseTagNameMock: string = "My-Tag-One,My-Tag-Two";
+    const artifactTagNameMock: string = "My-Artifact-Tag-One,My-Artifact-Tag-Two";
+    const sourceBranchNameMock: string = "My-Branch";
+    const stageStatusMock: string = "succeeded,rejected";
+
     const updateIntervalMock: string = "1";
     const approvalRetryMock: string = "1";
 
-    let inputs: {[key: string]: string};
+    let inputs: {[key: string]: string | boolean};
     let variables: {[key: string]: string};
 
     const taskHelper: ITaskHelper = new TaskHelper(debugCreatorMock.target);
@@ -42,8 +48,11 @@ describe("TaskHelper", ()  => {
         const getInputMock = ImportMock.mockFunction(TaskLibrary, "getInput");
         getInputMock.callsFake(i => { return inputs[i] || null; });
 
+        const getBoolInputMock = ImportMock.mockFunction(TaskLibrary, "getBoolInput");
+        getBoolInputMock.callsFake(i => { return (typeof inputs[i] === "boolean") ? inputs[i] : false; });
+
         const getDelimitedInputMock = ImportMock.mockFunction(TaskLibrary, "getDelimitedInput");
-        getDelimitedInputMock.callsFake(i => { return inputs[i] ? inputs[i].split(",") : []; });
+        getDelimitedInputMock.callsFake(i => { return inputs[i] ? inputs[i].toString().split(",") : []; });
 
         const getVariableMock = ImportMock.mockFunction(TaskLibrary, "getVariable");
         getVariableMock.callsFake(i => { return variables[i] || null; });
@@ -59,6 +68,60 @@ describe("TaskHelper", ()  => {
 
     });
 
+    it("Should get latest release parameters", async () => {
+
+        //#region ARRANGE
+
+        inputs["releaseStrategy"] = "latest";
+        inputs["projectName"] = projectNameMock;
+        inputs["definitionName"] = definitionNameMock;
+
+        inputs["releaseTagFilter"] = true;
+        inputs["releaseTagName"] = releaseTagNameMock;
+
+        inputs["artifactTagFilter"] = true;
+        inputs["artifactTagName"] = artifactTagNameMock;
+
+        inputs["sourceBranchFilter"] = true;
+        inputs["sourceBranchName"] = sourceBranchNameMock;
+
+        inputs["stageStatusFilter"] = true;
+        inputs["stageStatus"] = stageStatusMock;
+
+        inputs["updateInterval"] = updateIntervalMock;
+        inputs["approvalRetry"] = approvalRetryMock;
+
+        inputs["releaseStages"] = releaseStagesMock;
+
+        //#endregion
+
+        //#region ACT
+
+        const result = await taskHelper.getParameters();
+
+        //#endregion
+
+        //#region ASSERT
+
+        chai.expect(result).to.not.eq(null);
+        chai.expect(result.releaseType).to.eq("Latest");
+        chai.expect(result.projectName).to.eq(projectNameMock);
+        chai.expect(result.definitionName).to.eq(definitionNameMock);
+
+        chai.expect(result.settings.sleep).to.eq(Number(updateIntervalMock) * 1000);
+        chai.expect(result.settings.approvalRetry).to.eq(Number(approvalRetryMock));
+
+        chai.expect(result.stages).to.eql([ "DEV", "TEST", "PROD" ]);
+        chai.expect(result.filters.releaseTags).to.eql([ "My-Tag-One", "My-Tag-Two" ]);
+        chai.expect(result.filters.artifactTags).to.eql([ "My-Artifact-Tag-One", "My-Artifact-Tag-Two" ]);
+        chai.expect(result.filters.artifactBranch).to.eq(sourceBranchNameMock);
+        chai.expect(result.filters.stageStatuses).to.eql([ "succeeded", "rejected" ]);
+
+        //#endregion
+
+    });
+
+
     it("Should get specific release parameters", async () => {
 
         //#region ARRANGE
@@ -66,10 +129,12 @@ describe("TaskHelper", ()  => {
         inputs["releaseStrategy"] = "specific";
         inputs["projectName"] = projectNameMock;
         inputs["definitionName"] = definitionNameMock;
-        inputs["releaseName"] = releaseNameMock;
-        inputs["releaseStages"] = releaseStagesMock;
+
         inputs["updateInterval"] = updateIntervalMock;
         inputs["approvalRetry"] = approvalRetryMock;
+
+        inputs["releaseName"] = releaseNameMock;
+        inputs["releaseStages"] = releaseStagesMock;
 
         //#endregion
 
@@ -85,10 +150,12 @@ describe("TaskHelper", ()  => {
         chai.expect(result.releaseType).to.eq("Specific");
         chai.expect(result.projectName).to.eq(projectNameMock);
         chai.expect(result.definitionName).to.eq(definitionNameMock);
-        chai.expect(result.releaseName).to.eq(releaseNameMock);
-        chai.expect(result.stages).to.eql([ "DEV", "TEST", "PROD" ]);
+
         chai.expect(result.settings.sleep).to.eq(Number(updateIntervalMock) * 1000);
         chai.expect(result.settings.approvalRetry).to.eq(Number(approvalRetryMock));
+
+        chai.expect(result.releaseName).to.eq(releaseNameMock);
+        chai.expect(result.stages).to.eql([ "DEV", "TEST", "PROD" ]);
 
         //#endregion
 
