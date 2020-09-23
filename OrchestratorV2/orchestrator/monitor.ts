e5a72bb3b5ef6807d84624c35e67e9cf58d1f368
Dmitry Serbin <dmitry.serbin@iag.co.nz>
2020-09-23 13:11:45 +1200

    Bugfix

--- OrchestratorV2/orchestrator/monitor.ts
+++ OrchestratorV2/orchestrator/monitor.ts
@@ -141,7 +141,8 @@ export class Monitor implements IMonitor {
         stageProgress.id = stageStatus.id;
         stageProgress.release = stageStatus.release!.name;
         stageProgress.deployment = currentAttempt;
-        stageProgress.duration = stageStatus.timeToDeploy!.toLocaleString();
+        stageProgress.duration = stageStatus.timeToDeploy
+            ? stageStatus.timeToDeploy.toLocaleString() : undefined;
 
     }
 
