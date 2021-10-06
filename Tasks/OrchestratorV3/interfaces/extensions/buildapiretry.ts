34448bc7d5ee1b03cf9052a4f181ed2e3104843a
Dmitry Serbin <dmitry.serbin@iag.co.nz>
2021-10-06 13:05:08 +1300

    Update interfaces

--- Tasks/OrchestratorV3/interfaces/extensions/buildapiretry.ts
+++ Tasks/OrchestratorV3/interfaces/extensions/buildapiretry.ts
@@ -1,8 +1,9 @@
 import { IBuildApi } from "azure-devops-node-api/BuildApi";
-import { Build, BuildReason, BuildStatus, BuildResult, QueryDeletedOption, BuildQueryOrder } from "azure-devops-node-api/interfaces/BuildInterfaces";
+import { Build, BuildReason, BuildStatus, BuildResult, QueryDeletedOption, BuildQueryOrder, BuildDefinition } from "azure-devops-node-api/interfaces/BuildInterfaces";
 
 export interface IBuildApiRetry extends Partial<IBuildApi> {
 
+    getDefinition(project: string, definitionId: number, revision?: number, minMetricsTime?: Date, propertyFilters?: string[], includeLatestBuilds?: boolean): Promise<BuildDefinition>;
     getBuilds(project: string, definitions?: number[], queues?: number[], buildNumber?: string, minTime?: Date, maxTime?: Date, requestedFor?: string, reasonFilter?: BuildReason, statusFilter?: BuildStatus, resultFilter?: BuildResult, tagFilters?: string[], properties?: string[], top?: number, continuationToken?: string, maxBuildsPerDefinition?: number, deletedFilter?: QueryDeletedOption, queryOrder?: BuildQueryOrder, branchName?: string, buildIds?: number[], repositoryId?: string, repositoryType?: string): Promise<Build[]>;
 
 }
