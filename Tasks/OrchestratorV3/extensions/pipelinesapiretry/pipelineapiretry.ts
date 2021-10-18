52ebf77c544af0da814e8b06b27b237d8bc30fe8
Dmitry Serbin <dmitry.serbin@iag.co.nz>
2021-10-19 12:14:53 +1300

    Fix linter warnings

--- Tasks/OrchestratorV3/extensions/pipelinesapiretry/pipelineapiretry.ts
+++ Tasks/OrchestratorV3/extensions/pipelinesapiretry/pipelineapiretry.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { Build, BuildDefinition } from "azure-devops-node-api/interfaces/BuildInterfaces";
 
 import { IApiClient } from "../../common/iapiclient";
