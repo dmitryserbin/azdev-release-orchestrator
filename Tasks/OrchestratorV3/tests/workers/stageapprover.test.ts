cafa2b6c48f112df76c46dde642b086e72370715
Dmitry Serbin <dmitry.serbin@iag.co.nz>
2021-12-16 14:03:32 +1300

    Add tests

---
+++ Tasks/OrchestratorV3/tests/workers/stageapprover.test.ts
@@ -0,0 +1,126 @@
+import "mocha";
+
+import * as chai from "chai";
+import * as TypeMoq from "typemoq";
+import * as faker from "faker";
+
+import { Build, TimelineRecordState } from "azure-devops-node-api/interfaces/BuildInterfaces";
+
+import { ILogger } from "../../loggers/ilogger";
+import { IDebug } from "../../loggers/idebug";
+import { ICommonHelper } from "../../helpers/commonhelper/icommonhelper";
+import { ISettings } from "../../helpers/taskhelper/isettings";
+import { IBuildStage } from "../../workers/progressmonitor/ibuildstage";
+import { IStageSelector } from "../../helpers/stageselector/istageselector";
+import { IStageApprover } from "../../workers/stageapprover/istageapprover";
+import { StageApprover } from "../../workers/stageapprover/stageapprover";
+import { IBuildSelector } from "../../helpers/buildselector/ibuildselector";
+import { IBuildApproval } from "../../workers/progressmonitor/ibuildapproval";
+
+describe("StageApprover", async () => {
+
+    const loggerMock = TypeMoq.Mock.ofType<ILogger>();
+    const debugMock = TypeMoq.Mock.ofType<IDebug>();
+
+    loggerMock
+        .setup((x) => x.log(TypeMoq.It.isAny()))
+        .returns(() => null);
+
+    loggerMock
+        .setup((x) => x.extend(TypeMoq.It.isAnyString()))
+        .returns(() => debugMock.object);
+
+    debugMock
+        .setup((x) => x.extend(TypeMoq.It.isAnyString()))
+        .returns(() => debugMock.object);
+
+    const buildMock = {
+
+        buildNumber: faker.random.word(),
+        id: faker.datatype.number(),
+
+    } as Build;
+
+    let settingsMock: ISettings;
+    let stageOneMock: IBuildStage;
+    let approvalOneMock: IBuildApproval;
+
+    const commonHelperMock = TypeMoq.Mock.ofType<ICommonHelper>();
+    const buildSelectorMock = TypeMoq.Mock.ofType<IBuildSelector>();
+    const stageSelectorMock = TypeMoq.Mock.ofType<IStageSelector>();
+
+    const stageApprover: IStageApprover = new StageApprover(buildSelectorMock.object, stageSelectorMock.object, commonHelperMock.object, loggerMock.object);
+
+    beforeEach(async () => {
+
+        commonHelperMock.reset();
+        buildSelectorMock.reset();
+        stageSelectorMock.reset();
+
+        settingsMock = {
+
+            proceedSkippedStages: false,
+            updateInterval: 1,
+
+        } as ISettings;
+
+        approvalOneMock = {
+
+            id: faker.random.word(),
+            state: TimelineRecordState.Pending,
+            result: null,
+
+        } as IBuildApproval;
+
+        stageOneMock = {
+
+            id: faker.random.word(),
+            name: faker.random.word(),
+            state: TimelineRecordState.Pending,
+            attempt: {
+                approval: 0,
+                check: 0,
+            },
+            approvals: [ approvalOneMock ],
+
+        } as IBuildStage;
+
+    });
+
+    it("Should approve stage", async () => {
+
+        //#region ARRANGE
+
+        const approvedResult = { status: `approved` };
+
+        stageSelectorMock
+            .setup((x) => x.approveStage(buildMock, approvalOneMock, undefined))
+            .returns(() => Promise.resolve(approvedResult))
+            .verifiable(TypeMoq.Times.once());
+
+        //#endregion
+
+        //#region ACT
+
+        const result = await stageApprover.approve(stageOneMock, buildMock, settingsMock);
+
+        //#endregion
+
+        //#region ASSERT
+
+        chai.expect(result).to.not.eq(null);
+
+        stageSelectorMock.verifyAll();
+
+        //#endregion
+
+    });
+
+});
+
+process.on("unhandledRejection", (error: unknown) => {
+
+    console.error(error);
+    process.exit(1);
+
+});
